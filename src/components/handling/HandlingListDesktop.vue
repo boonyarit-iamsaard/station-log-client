d92127d67cc1f27259c5cfd6aac7d626bc932aa7
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-05-22 20:41:04 +0700

    Fix task details dialog

--- src/components/handling/HandlingListDesktop.vue
+++ src/components/handling/HandlingListDesktop.vue
@@ -1,75 +1,76 @@
 <template>
-  <v-data-table
-    class="mx-auto"
-    style="max-width: 959px"
-    :headers="headers"
-    :items="handlingRecords"
-    :sort-by="['date']"
-    :sort-desc="[true]"
-  >
-    <template v-slot:top v-if="$vuetify.breakpoint.mdAndUp">
-      <div class="d-flex justify-end pa-4">
-        <v-btn depressed link color="primary" to="/handling/create">
-          <v-icon left>mdi-plus</v-icon>
-          ADD
-        </v-btn>
-      </div>
-    </template>
-
-    <template v-slot:item.date="{ item }">
-      {{ setDateFormatHandler(item.date) }}
-    </template>
-
-    <template v-slot:item.airline="{ item }">
-      <span v-if="item.airline === 'Other'">
-        {{ item.otherAirline }}
-      </span>
-      <span v-else>
-        {{ item.airline }}
-      </span>
-    </template>
-
-    <template v-slot:item.check="{ item }">
-      <span v-if="item.check === 'Other'">
-        {{ item.otherCheck }}
-      </span>
-      <span v-else>
-        {{ item.check }}
-      </span>
-    </template>
-
-    <template v-slot:item.tasks="{ item }">
-      <v-dialog v-model="dialog" max-width="959">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-            @click="setTaskDetailsDialogHandler(item)"
-          >
-            <v-icon>mdi-open-in-new</v-icon>
+  <div>
+    <v-data-table
+      class="mx-auto"
+      style="max-width: 959px"
+      :headers="headers"
+      :items="handlingRecords"
+      :sort-by="['date']"
+      :sort-desc="[true]"
+    >
+      <template v-slot:top v-if="$vuetify.breakpoint.mdAndUp">
+        <div class="d-flex justify-end pa-4">
+          <v-btn depressed link color="primary" to="/handling/create">
+            <v-icon left>mdi-plus</v-icon>
+            ADD
           </v-btn>
-        </template>
+        </div>
+      </template>
 
-        <v-card>
-          <v-card-title class="pa-4">Tasks</v-card-title>
-          <v-card-text class="pa-4">
-            <v-data-table
-              :items="taskDetails"
-              :items-per-page="5"
-              :headers="taskHeaders"
-            ></v-data-table>
-          </v-card-text>
-        </v-card>
-      </v-dialog>
-    </template>
+      <template v-slot:item.date="{ item }">
+        {{ setDateFormatHandler(item.date) }}
+      </template>
 
-    <template v-slot:item.actions="{ item }">
-      <v-btn class="mr-2" icon link :to="`/handling/edit/${item._id}`">
-        <v-icon> mdi-pencil </v-icon>
-      </v-btn>
-    </template>
-  </v-data-table>
+      <template v-slot:item.airline="{ item }">
+        <span v-if="item.airline === 'Other'">
+          {{ item.otherAirline }}
+        </span>
+        <span v-else>
+          {{ item.airline }}
+        </span>
+      </template>
+
+      <template v-slot:item.check="{ item }">
+        <span v-if="item.check === 'Other'">
+          {{ item.otherCheck }}
+        </span>
+        <span v-else>
+          {{ item.check }}
+        </span>
+      </template>
+
+      <template v-slot:item.tasks="{ item }">
+        <v-btn icon @click.stop="setTaskDetailsDialogHandler(item)">
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-btn class="mr-2" icon link :to="`/handling/edit/${item._id}`">
+          <v-icon> mdi-pencil </v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="dialog" max-width="959" persistent>
+      <v-card>
+        <v-card-title class="pa-4">Tasks</v-card-title>
+        <v-card-text class="pa-4">
+          <v-data-table
+            :items="taskDetails"
+            :items-per-page="5"
+            :headers="taskHeaders"
+          ></v-data-table>
+        </v-card-text>
+
+        <v-card-actions class="pa-4">
+          <v-spacer />
+
+          <v-btn color="primary" text @click="dialog = false">CLOSE</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -114,10 +115,15 @@ export default {
     },
 
     setTaskDetailsDialogHandler(item) {
-      this.taskDetails = item.tasks.map(task => {
-        return { ...task, taskNo: !task.taskNo ? 'N/A' : task.taskNo };
-      });
+      let items = [];
+      // this.taskDetails = item.tasks.map(task => {
+      //   return { ...task, taskNo: !task.taskNo ? 'N/A' : task.taskNo };
+      // });
+      item.tasks.forEach(task =>
+        items.push({ ...task, taskNo: !task.taskNo ? 'N/A' : task.taskNo })
+      );
 
+      this.taskDetails = items;
       this.dialog = true;
     },
   },
