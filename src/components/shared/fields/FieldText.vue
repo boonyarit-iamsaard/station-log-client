7d655c16f874b1533628dfffb264995c842230b9
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-06-15 22:21:33 +0700

    feat(common): add reuseable form fields

---
+++ src/components/shared/fields/FieldText.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-form ref="form">
+    <v-text-field
+      :label="fields.label"
+      :prefix="fields.prefix"
+      :rules="fields.rules"
+      v-model="model"
+      dense
+      outlined
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'FieldText',
+
+  props: ['fields', 'value'],
+
+  methods: {
+    validateField() {
+      this.$refs.form.$nextTick(() => {
+        this.$refs.form.validate();
+      });
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        value = value.toUpperCase();
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/shared/fields/FieldTime.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-form ref="form">
+    <v-text-field
+      :label="fields.label"
+      :rules="fields.rules"
+      append-icon="mdi-clock-outline"
+      dense
+      hint="HH:mm"
+      outlined
+      persistent-hint
+      v-mask="'##:##'"
+      v-model="model"
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+import { VueMaskDirective } from 'v-mask';
+
+export default {
+  name: 'FieldTime',
+
+  props: ['fields', 'value'],
+
+  directives: {
+    mask: VueMaskDirective,
+  },
+
+  methods: {
+    validateField() {
+      this.$refs.form.$nextTick(() => {
+        this.$refs.form.validate();
+      });
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
