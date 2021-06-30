452029ada6209c830faab69600efe72c1aeb9e85
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-06-30 20:30:31 +0700

    improve(services): add error interceptor

--- src/services/http-service/index.js
+++ src/services/http-service/index.js
@@ -18,6 +18,24 @@ const authInterceptor = config => {
   return config;
 };
 
+const errorInterceptors = error => {
+  if (error.response) {
+    const { data } = error.response;
+    const { message } = data;
+
+    throw new Error(message);
+  }
+
+  if (error.request) {
+    throw new Error(
+      'Could not connect to server, please contact your administrator or try again later.'
+    );
+  }
+
+  throw new Error(error.message);
+};
+
 HTTP.interceptors.request.use(authInterceptor);
+HTTP.interceptors.response.use(response => response, errorInterceptors);
 
 export default HTTP;
