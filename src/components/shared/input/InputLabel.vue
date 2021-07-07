7770493543780182b87bd8c160861d46d159287d
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-07-07 18:52:02 +0700

    refactor(shared): add InputLabel

---
+++ src/components/shared/input/InputLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="caption grey--text font-weight-medium">
+    {{ label }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'InputLabel',
+
+  props: {
+    label: {
+      type: String,
+      default: 'Label',
+    },
+  },
+};
+</script>
+
+<style scoped></style>
