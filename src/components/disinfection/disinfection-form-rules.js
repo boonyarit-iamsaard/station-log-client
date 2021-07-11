89f3e2c3ed187e0bb8fffdcd6a8864fc0519d60e
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-07-11 09:44:25 +0700

    fix(disinfection): remove start time and end time validation

--- src/components/disinfection/disinfection-form-rules.js
+++ src/components/disinfection/disinfection-form-rules.js
@@ -3,18 +3,10 @@ export const disinfectionFormRules = {
   fltno: [v => !!v || '* Flt No. is required.'],
   tail: [v => !!v || '* A/C Reg. is required.'],
   aircraftType: [v => !!v || '* A/C type is required.'],
-  startAt: [
-    v => !!v || '* Start time is required.',
-    v => v.length >= 5 || '* Please enter in HH:mm format.',
-  ],
-  endAt: [
-    v => !!v || '* End time is required.',
-    v => v.length >= 5 || '* Please enter in HH:mm format.',
-  ],
   mechanic1: [v => !!v || '* Mechanic-1 is required.'],
   mechanic2: [v => !!v || '* Mechanic-2 is required.'],
   chemicalUsage: [
     v => !!v || '* Used (litters) is required.',
-    v => v > 0 || 'Used must not be zero.',
+    v => v >= 1 || '* Used must not be zero.',
   ],
 };
