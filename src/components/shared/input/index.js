63d8d9f676fc3d758f8f57312f47897b51b2ba86
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-09-03 13:56:08 +0700

    refactor: re-export shared input

--- src/components/shared/input/index.js
+++ src/components/shared/input/index.js
@@ -1,1 +1,8 @@
-export { default as InputAutocomplete } from '@/components/shared/input/InputAutocomplete';
+export { default as InputAutocomplete } from '@/components/shared/input/InputAutocomplete.vue';
+export { default as InputCheckbox } from '@/components/shared/input/InputCheckbox.vue';
+export { default as InputCombo } from '@/components/shared/input/InputCombo.vue';
+export { default as InputDate } from '@/components/shared/input/InputDate.vue';
+export { default as InputLabel } from '@/components/shared/input/InputLabel.vue';
+export { default as InputSelect } from '@/components/shared/input/InputSelect.vue';
+export { default as InputText } from '@/components/shared/input/InputText.vue';
+export { default as InputTextarea } from '@/components/shared/input/InputTextarea.vue';
