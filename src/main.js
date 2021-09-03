90c799c20a0cf984657dcd8e16b46eb1ef495a6b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2021-09-03 13:56:39 +0700

    refactor: register shared components globally

--- src/main.js
+++ src/main.js
@@ -7,6 +7,28 @@ import vuetify from './plugins/vuetify';
 
 import '@/assets/styles/index.scss';
 
+// Import shared components
+import {
+  InputAutocomplete,
+  InputCheckbox,
+  InputCombo,
+  InputDate,
+  InputLabel,
+  InputSelect,
+  InputText,
+  InputTextarea,
+} from '@/components/shared/input';
+
+// Register shared components globally
+Vue.component('input-date', InputDate);
+Vue.component('input-autocomplete', InputAutocomplete);
+Vue.component('input-checkbox', InputCheckbox);
+Vue.component('input-combo', InputCombo);
+Vue.component('input-label', InputLabel);
+Vue.component('input-select', InputSelect);
+Vue.component('input-text', InputText);
+Vue.component('input-textarea', InputTextarea);
+
 Vue.config.productionTip = false;
 
 new Vue({
